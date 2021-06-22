14392a1f2c0c08ae79e07b7ea047f840a3a83bb7
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-06-22 21:07:32 +1000

    Added neovim nightly build scripts

--- install/installer.sh
+++ install/installer.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
 
+get_platform() {
+  case "$(uname -s)" in
+    Linux*)  platform=Linux   ;;
+    Darwin*) platform=Darwin  ;;
+    CYGWIN*) platform=Cygwin  ;;
+    MINGW*)  platform=MinGw   ;;
+    *)       platform=Unknown ;;
+  esac
+}
+
+
+install_packer() {
+  if [ ! -d "$HOME/.local/share/nvim/site/pack/packer" ]; then
+    printf "Installing packer"
+    git clone https://github.com/wbthomason/packer.nvim \
+      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    printf "\npacker installed!\n"
+  fi
+}
+
+
+install_neovim_nightly() {
+  echo 'Installing neovim nightly builds from appimage...'
+
+  wget --quiet --directory-prefix=$HOME https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+  chmod +x $HOME/nvim.appimage
+  ./$HOME/nvim.appimage --appimage-extract
+  mv sqashfs-root $HOME/sqashfs-root
+
+  # TODO: Rename AppRun to nvim
+
+  sudo ln -sf $HOME/squashfs-root/AppRun /usr/local/bin/nvim
+}
+
+
 colorme() {
   RED=$(printf '\033[31m')
   GREEN=$(printf '\033[32m')
@@ -32,9 +67,10 @@ error() {
 
 
 main() {
-
+  echo "starting main"
   colorme
 
+  echo "here"
 
   for opt in "$@"; do
     case $opt in
@@ -52,10 +88,9 @@ main() {
     esac
   done
 
+  get_platform
+  install_packer
 }
 
-
 main
-
-
 
