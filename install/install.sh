905d5be6f051c459e127e34e8700a4df9859d7e9
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-07-06 22:38:50 +1000

    Updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR=new-dotfiles
-DOTFILES_PATH=$HOME/DOTFILES_DIR
+DOTFILES_PATH=$HOME/$DOTFILES_DIR
 if [ -d "$DOTFILES_PATH" ]; then
   rm -rf "$DOTFILES_PATH"
 fi
@@ -12,7 +12,7 @@ INSTALLER_BIN=dotfiles
 INSTALLER_PATH=/usr/local/sbin/$INSTALLER_BIN
 
 chmod +x $DOTFILES_PATH/install/installer.sh
-ln -sf $DOTFILES_PATH/install/installer.sh $INSTALLER_PATH
+sudo ln -sf $DOTFILES_PATH/install/installer.sh $INSTALLER_PATH
 
 echo "Installed at $INSTALLER_PATH"
 
