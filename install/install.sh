990626f6edf23555364397b62cec3cb94b40b2d2
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-07-06 22:37:06 +1000

    Updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,3 +8,11 @@ fi
 
 git clone https://github.com/jdansev/dotfiles-v3.0.git $DOTFILES_PATH
 
+INSTALLER_BIN=dotfiles
+INSTALLER_PATH=/usr/local/sbin/$INSTALLER_BIN
+
+chmod +x $DOTFILES_PATH/install/installer.sh
+ln -sf $DOTFILES_PATH/install/installer.sh $INSTALLER_PATH
+
+echo "Installed at $INSTALLER_PATH"
+
