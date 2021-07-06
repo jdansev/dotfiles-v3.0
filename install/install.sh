d1385fc707a31a186e2b23a630a0b0cc03d5930d
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-07-06 22:30:09 +1000

    Updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DOTFILES_DIR=$HOME/new-dotfiles
-if [ -d "$DOTFILES_DIR" ]; then
-  rm -rf "$DOTFILES_DIR"
+DOTFILES_DIR=new-dotfiles
+DOTFILES_PATH=$HOME/DOTFILES_DIR
+if [ -d "$DOTFILES_PATH" ]; then
+  rm -rf "$DOTFILES_PATH"
 fi
 
-git clone https://github.com/jdansev/dotfiles-v3.0.git $DOTFILES_DIR
-
+git clone https://github.com/jdansev/dotfiles-v3.0.git $DOTFILES_PATH
 
