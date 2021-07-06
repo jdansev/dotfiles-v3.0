89b2ade1cfbd9149bf8f90e7c930ba5793b19e87
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-07-06 22:25:38 +1000

    Updated install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Hello, World!"
-echo "Arguments passed: $@"
+DOTFILES_DIR=$HOME/new-dotfiles
+if [ -d "$DOTFILES_DIR" ]; then
+  rm -rf "$DOTFILES_DIR"
+fi
+
+git clone https://github.com/jdansev/dotfiles-v3.0.git $DOTFILES_DIR
+
+
