41160d9708dc477d7dee3c8ba06f64ce1ecd0eda
John Daniel Sevilla <jdsevilla1996@gmail.com>
2021-07-07 22:38:15 +1000

    Refactored directory structure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-DOTFILES_DIR=new-dotfiles
+DOTFILES_DIR=.dotfiles
 DOTFILES_PATH=$HOME/$DOTFILES_DIR
+INSTALLER_BIN=dotfiles
+INSTALLER_PATH=/usr/local/sbin/$INSTALLER_BIN
+
 if [ -d "$DOTFILES_PATH" ]; then
   rm -rf "$DOTFILES_PATH"
 fi
 
 git clone https://github.com/jdansev/dotfiles-v3.0.git $DOTFILES_PATH
-
-INSTALLER_BIN=dotfiles
-INSTALLER_PATH=/usr/local/sbin/$INSTALLER_BIN
 
 chmod +x $DOTFILES_PATH/install/installer.sh
 sudo ln -sf $DOTFILES_PATH/install/installer.sh $INSTALLER_PATH
